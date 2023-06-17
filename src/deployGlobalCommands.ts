1d16cfecbaa38459600899fd45e3a060215f1244
Deon Daigh <deon.daigh@gmail.com>
2023-06-16 20:41:00 -0500

    added clientId to deployGlobalCommands.ts

--- src/deployGlobalCommands.ts
+++ src/deployGlobalCommands.ts
@@ -1,10 +1,10 @@
 const { REST, Routes } = require('discord.js');
-const { clientId} = require('../config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 import "dotenv/config";
 
 const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
